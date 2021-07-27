b24c23fe82fd310ad5b21db4e1c05a7e3c884616
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2021-07-27 11:46:24 +0100

    Finish MNIST experiment.

--- mnist.py
+++ mnist.py
@@ -34,10 +34,13 @@ def train(net, train_data):
 
     ts = np.eye(10)
 
-    for row in train_data:
+    for i, row in enumerate(train_data):
+        if i % 1000 == 0:
+            print(i)
+
         digit = row[0]
         x = to_col(row[1:])
-        t = to_col(ts[digit, :])
+        t = to_col(ts[int(digit), :])
         net.train(x, t)
 
 
@@ -55,5 +58,9 @@ if __name__ == "__main__":
     print(f"Accuracy: {round(100 * accuracy, 2)}%")
 
     # Train the network.
+    train_data = load_data("mnistdata/mnist_train.csv")
+    train(net, train_data)
 
     # Test the network again.
+    accuracy = test(net, test_data)
+    print(f"Accuracy: {round(100 * accuracy, 2)}%")
