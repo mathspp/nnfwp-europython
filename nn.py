28461e89c8709faa1b3c4111aa73d0235fdddf80
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2021-07-27 09:20:19 +0100

    Create weights matrix and bias vector.

--- nn.py
+++ nn.py
@@ -1,6 +1,19 @@
 import numpy as np
 
 
+def create_weights_matrix(nrows, ncols):
+    """Generate normally distributed random numbers."""
+    return np.random.default_rng().normal(
+        loc=0,
+        scale=1/(nrows*ncols),
+        size=(nrows, ncols),
+    )
+
+
+def create_bias_vector(length):
+    return create_weights_matrix(length, 1)
+
+
 class Layer:
     """Class representing the connections between two layers of neurons."""
 
